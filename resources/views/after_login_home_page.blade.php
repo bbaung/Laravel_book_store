19d3690c6545cfbaf396b1e6df281c9f68cd82e3
Dustin Moe <nidanmoethet@gmail.com>
2023-11-24 05:05:10 +0000

    Update file after_login_home_page.blade.php

--- resources/views/after_login_home_page.blade.php
+++ resources/views/after_login_home_page.blade.php
@@ -1,7 +1,193 @@
 <h1>
 				After login home page
 </h1>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Use with CDN Link</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <style>
+    .col-2:hover {
+      transform: scale(1.05);
+      transition: all 0.5s ease;
+    }
+
+    .col-2 {
+      transition: transform 0.3s ease;
+    }
+
+    body {
+      height: 100%;
+      margin: 0;
+      padding: 0;
+    }
+
+    .wrapper {
+      min-height: 100%;
+      margin-bottom: -50px;
+      /* Height of the footer */
+    }
+
+    .footer {
+      height: 75px;
+      padding: 15px 0;
+      position: relative;
+      bottom: 0;
+      width: 100%;
+    }
+
+    .navbar-brand {
+      text-decoration: none;
+      transition: text-decoration 0.5s ease;
+    }
+
+    .navbar-brand:hover {
+      text-decoration: underline;
+    }
+
+    .main-body {
+      padding-top: 140px;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="wrapper d-flex flex-column">
+    <div class="w-100 mb-5 fixed-top" style="z-index: 1;">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary pt-3">
+        <div class="container-fluid">
+          <a class="navbar-brand ms-5" href="#">Book Shop</a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <form class="d-flex mx-auto w-50" role="search">
+              <input class="form-control me-2" type="" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-secondary" type="submit">Search</button>
+            </form>
+          </div>
+
+          <div class="nav-item mx-auto me-5">
+            <button type="button" class="btn btn-outline-secondary">
+              <i class="fas fa-user-circle me-1"></i> My Account
+            </button>
+          </div>
+          <div class="nav-item mx-auto me-5">
+            <button type="button" class="btn btn-outline-secondary"><i
+                class="fa-sharp fa-solid fa-cart-shopping fa-beat-fade"></i> My Cart</button>
+          </div>
+        </div>
+      </nav>
+
+      <div class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid align-self-center w-75">
+          <a class="navbar-brand" href="#">Fiction</a>
+          <a class="navbar-brand" href="#">Science</a>
+          <a class="navbar-brand" href="#">Action</a>
+          <a class="navbar-brand" href="#">Romance</a>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid align-self-center w-75 bg-body-tertiary main-body">
+      <div class="row">
+        <h3 class="d-inline col-6">Best Selling Books</h3>
+        <a class="d-inline-block fs-5 text-end col-6" href="#">View All</a>
+      </div>
+      <div class="row my-5">
+        <div class="col-2">
+          <div class="card" style="width: 7rem;">
+            <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg" class="card-img-top"
+              alt="...">
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card" style="width: 7rem;">
+            <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg" class="card-img-top"
+              alt="...">
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card" style="width: 7rem;">
+            <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg" class="card-img-top"
+              alt="...">
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card" style="width: 7rem;">
+            <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg" class="card-img-top"
+              alt="...">
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card" style="width: 7rem;">
+            <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg" class="card-img-top"
+              alt="...">
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="card" style="width: 7rem;">
+            <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg" class="card-img-top"
+              alt="...">
+          </div>
+        </div>
+        <div class="row mt-5">
+          <h3 class="d-inline col-6">Best Reviewed Books</h3>
+          <a class="d-inline-block fs-5 text-end col-6" href="#">View All</a>
+        </div>
+        <div class="row my-5">
+          <div class="col-2">
+            <div class="card" style="width: 7rem;">
+              <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg"
+                class="card-img-top" alt="...">
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="card" style="width: 7rem;">
+              <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg"
+                class="card-img-top" alt="...">
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="card" style="width: 7rem;">
+              <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg"
+                class="card-img-top" alt="...">
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="card" style="width: 7rem;">
+              <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg"
+                class="card-img-top" alt="...">
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="card" style="width: 7rem;">
+              <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg"
+                class="card-img-top" alt="...">
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="card" style="width: 7rem;">
+              <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg"
+                class="card-img-top" alt="...">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  <footer class="footer mt-auto bg-info-subtle">
+    <div class="container text-center">
+      <span class="text-muted">&copy; 2023 Bookshop.com. All Rights Reserved.</span>
+    </div>
+  </footer>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</html>
 <div>
 				<!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
 </div>
