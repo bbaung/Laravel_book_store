a9cc3e751839b784968234e4ad7e4c3f7d20cde7
Dustin Moe <nidanmoethet@gmail.com>
2023-11-24 05:06:34 +0000

    Update file book_review.blade.php

--- resources/views/book_review.blade.php
+++ resources/views/book_review.blade.php
@@ -1,7 +1,129 @@
 <h1>
 				Book review
 </h1>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book_Review</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+
+        .wrapper {
+            min-height: 100%;
+            margin-bottom: -100px;
+            /* Height of the footer */
+        }
+
+        .footer {
+      background-color: #f8f9fa;
+      padding: 20px 0;
+      text-align: center;
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      height: 60px; 
+        }
+
+        .navbar-brand {
+            text-decoration: none;
+            transition: text-decoration 0.5s ease;
+        }
+
+        .navbar-brand:hover {
+            text-decoration: underline;
+        }
+
+        .main-body {
+            padding-top: 180px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper d-flex flex-column">
+        <div class="w-100 mb-5 fixed-top bg-body-tertiary" style="z-index: 1;">
+            <nav class="navbar navbar-expand-lg pt-3">
+                <div class="container-fluid">
+                    <a class="navbar-brand ms-5" href="#">Book Shop</a>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <form class="d-flex mx-auto w-50" role="search">
+                            <input class="form-control me-2" type="" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-secondary" type="submit">Search</button>
+                        </form>
+                    </div>
+
+                    <div class="nav-item mx-auto me-5">
+                        <button type="button" class="btn btn-outline-secondary">Sign In</button>
+                    </div>
+                    <div class="nav-item mx-auto me-5">
+                        <button type="button" class="btn btn-outline-secondary"><i
+                                class="fa-sharp fa-solid fa-cart-shopping fa-beat-fade"></i> My Cart</button>
+                    </div>
+                </div>
+            </nav>
+
+            <div class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid align-self-center w-75">
+                    <a class="navbar-brand" href="#">Fiction</a>
+                    <a class="navbar-brand" href="#">Science</a>
+                    <a class="navbar-brand" href="#">Action</a>
+                    <a class="navbar-brand" href="#">Romance</a>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid align-self-center w-75 main-body">
+            <div class="row">
+                <h3 class="d-inline col-6">Book Name</h3>
+                <a class="d-inline-block fs-5 text-end col-6" href="#">Home Menu</a>
+            </div>
+            <div class="row pt-3">
+                <div class="col-3">
+                    <div class="card" style="width: 17rem;">
+                        <img src="https://i.pinimg.com/originals/20/2b/18/202b1875a6513529150b232668ce0a4d.jpg"
+                            class="card-img-top" alt="...">
+                    </div>
+                </div>
+                <div class="col-9">
+                    <div class="card w-100 h-100">
+                        <div class="card-body">
+                            <h4 class="card-title">Card title</h4>
+                            <p class="card-text">In YOU CAN, YOU WILL, Joel Osteen shares eight undeniable qualities of
+                                winners that can help you to reach your potential and achieve new levels of success in your
+                                life. You are created to be a winner. Get these eight principles deep down on the inside and
+                                boldly go in the direction of your destiny.</p>
+                                <div class=" position-absolute bottom-0 d-flex">
+                                    <button type="button" class="btn btn-sm btn-secondary mx-2 my-3" disabled>2000 mmk</button>
+                                    <button type="button" class="btn btn-sm btn-secondary mx-2 my-3" disabled>100 stock</button>
+                                    <button type="button" class="btn btn-sm btn-outline-success mx-2 my-3">Add to cart</button>
+                
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div> 
+ </div>
+    <footer class="footer mt-auto bg-info-subtle">
+        <div class="container text-center">
+          <span class="text-muted">&copy; 2023 Bookshop.com. All Rights Reserved.</span>
+        </div>
+      </footer>
+</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+</html>
 <div>
 				<!-- Simplicity is an acquired taste. - Katharine Gerould -->
 </div>
