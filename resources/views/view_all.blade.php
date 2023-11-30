d1f56a58a8835e360597d9ecf06ce66c60913173
aung <bbaung4109@gmail.com>
2023-11-30 11:25:08 +0630

    view all

--- resources/views/view_all.blade.php
+++ resources/views/view_all.blade.php
@@ -5,3 +5,111 @@
 <div>
 				<!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
 </div>
+
+<h1>
+				Book review
+</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+				<head>
+								<meta charset="UTF-8">
+								<meta name="viewport" content="width=device-width, initial-scale=1.0">
+								<title>Book_Review</title>
+								<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+								<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+												integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+								<style>
+												body {
+																height: 100%;
+																margin: 0;
+																padding: 0;
+												}
+
+												.wrapper {
+																min-height: 100%;
+																margin-bottom: -100px;
+																/* Height of the footer */
+												}
+
+												.footer {
+																background-color: #f8f9fa;
+																padding: 20px 0;
+																text-align: center;
+																position: absolute;
+																bottom: 0;
+																width: 100%;
+																height: 60px;
+												}
+
+												.navbar-brand {
+																text-decoration: none;
+																transition: text-decoration 0.5s ease;
+												}
+
+												.navbar-brand:hover {
+																text-decoration: underline;
+												}
+
+												.main-body {
+																padding-top: 180px;
+												}
+								</style>
+				</head>
+
+				<body>
+								<div class="wrapper d-flex flex-column">
+												<div class="w-100 fixed-top bg-body-tertiary mb-5" style="z-index: 1;">
+																<nav class="navbar navbar-expand-lg pt-3">
+																				<div class="container-fluid">
+																								<a class="navbar-brand ms-5" href="#">Book Shop</a>
+																								<div class="navbar-collapse collapse" id="navbarSupportedContent">
+																												<form class="d-flex w-50 mx-auto" role="search">
+																																<input class="form-control me-2" type="" placeholder="Search"
+																																				aria-label="Search">
+																																<button class="btn btn-outline-secondary" type="submit">Search</button>
+																												</form>
+																								</div>
+
+																								<div class="nav-item mx-auto me-5">
+																												<button type="button" class="btn btn-outline-secondary">Sign In</button>
+																								</div>
+																								<div class="nav-item mx-auto me-5">
+																												<button type="button" class="btn btn-outline-secondary"><i
+																																				class="fa-sharp fa-solid fa-cart-shopping fa-beat-fade"></i> My Cart</button>
+																								</div>
+																				</div>
+																</nav>
+
+																<div class="navbar navbar-expand-lg navbar-light bg-light">
+																				<div class="container-fluid align-self-center w-75">
+																								<a class="navbar-brand" href="#">Fiction</a>
+																								<a class="navbar-brand" href="#">Science</a>
+																								<a class="navbar-brand" href="#">Action</a>
+																								<a class="navbar-brand" href="#">Romance</a>
+																				</div>
+																</div>
+												</div>
+												<div class="container-fluid align-self-center w-75 main-body">
+																<div class="row">
+																				<h3 class="d-inline col-6">All Books</h3>
+																				<a class="d-inline-block fs-5 col-6 text-end" href="/">Home Menu</a>
+																</div>
+
+												</div>
+
+								</div>
+								<footer class="footer bg-info-subtle mt-auto">
+												<div class="container text-center">
+																<span class="text-muted">&copy; 2023 Bookshop.com. All Rights Reserved.</span>
+												</div>
+								</footer>
+				</body>
+				<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+								integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+				</script>
+
+</html>
+<div>
+				<!-- Simplicity is an acquired taste. - Katharine Gerould -->
+</div>
